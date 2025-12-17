fe416f9537c8ce6140a83a0456638c5270299bb9
Harit Patel <haritpatel789@gmail.com>
2025-12-17 16:04:25 +0530

    [Harit Patel].Add Problems

--- core-java-practice/gcr-codebase/programming-elements/Hello.java
+++ core-java-practice/gcr-codebase/programming-elements/Hello.java
@@ -1,5 +1,5 @@
-class Main{
-   public static void main(String[] args){
-    System.out.println("Welcome to Bridgelabz!");
-	}
- }
+class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Bridgelabz!");
+    }
+}
--- core-java-practice/gcr-codebase/programming-elements/VolumeOfCylinder.java
+++ core-java-practice/gcr-codebase/programming-elements/VolumeOfCylinder.java
@@ -5,8 +5,10 @@ class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Radius : ");
         int radius = sc.nextInt();
+        System.out.print("Height : ");
+        int height = sc.nextInt();
         double pie = 3.14;
-        double area = pie * (radius * radius);
-        System.out.print("Area : " + area);
+        double volume = pie * (radius * radius) * height;
+        System.out.print("Volume : " + volume);
     }
 }
