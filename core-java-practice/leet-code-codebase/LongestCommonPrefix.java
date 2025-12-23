a004d5187d61dbfc53a9c90d69377719e0f19a59
Harit Patel <haritpatel789@gmail.com>
2025-12-23 14:39:55 +0530

    [Harit Patel].Add Leetcode problems

---
+++ core-java-practice/leet-code-codebase/LongestCommonPrefix.java
@@ -0,0 +1,20 @@
+import java.util.*;
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder result = new StringBuilder();
+        Arrays.sort(strs);
+        String first = strs[0];
+        String last = strs[strs.length -1];
+        
+
+        for(int i =0 ; i<first.length() ; i++){
+            if(first.charAt(i) == last.charAt(i)){
+                result.append(first.charAt(i));
+            }
+            else{
+                break;
+            }
+        }
+        return result.toString();
+    }
+}
