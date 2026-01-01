08a3c5a421af1e3f557909ac4c53795ad063c9e6
Harit Patel <haritpatel789@gmail.com>
2026-01-01 23:07:01 +0530

    [Harit Patel].Add scenario based problems

---
+++ dsa-practice/scenario-based/SentenceForamtter.java
@@ -0,0 +1,48 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class SentenceForamtter {
+	public static void main(String args[]) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		String result = sentenceFormatter(str);
+		System.out.println(result);
+	}
+
+	public static String sentenceFormatter(String str) {
+		str = str.trim().replaceAll("\\s+", " ");
+		StringBuilder sb = new StringBuilder();
+		boolean isCapital = true;
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (Character.isLetter(ch)) {
+				if (isCapital) {
+					sb.append(Character.toUpperCase(ch));
+					isCapital = false;
+				} else {
+					sb.append(Character.toLowerCase(ch));
+				}
+			}
+
+			else if (ch == '.' || ch == '!' || ch == '?') {
+				sb.append(ch);
+				sb.append(" ");
+				isCapital = true;
+			}
+
+			else if (ch == ',') {
+				sb.append(ch);
+				sb.append(" ");
+			}
+
+			else if (ch == ' ') {
+				if (sb.length() > 0 && sb.charAt(sb.length() - 1) != ' ') {
+					sb.append(" ");
+				}
+			}
+		}
+		String result = sb.toString().trim();
+		return result;
+	}
+}
---
+++ dsa-practice/scenario-based/TextAnalyzer.java
@@ -0,0 +1,70 @@
+package dsa;
+
+import java.util.Scanner;
+
+public class TextAnalyzer {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String paragraph = sc.nextLine();
+        paragraph = normalizeSpaces(paragraph);
+
+        if (paragraph.isEmpty()) {
+            System.out.println("Paragraph is empty.");
+            return;
+        }
+
+        String[] words = splitWords(paragraph);
+
+        printWordCount(words);
+        printLongestWord(words);
+
+        String oldWord = sc.next();
+        String newWord = sc.next();
+
+        String updatedParagraph = replaceWord(words, oldWord, newWord);
+        System.out.println(updatedParagraph);
+    }
+
+    private static String normalizeSpaces(String text) {
+        return text.trim().replaceAll("\\s+", " ");
+    }
+
+    private static String[] splitWords(String text) {
+        return text.split(" ");
+    }
+
+    private static void printWordCount(String[] words) {
+        System.out.println("total number of words : " + words.length);
+    }
+
+    private static void printLongestWord(String[] words) {
+        int maxLength = 0;
+        String maxWord = "";
+
+        for (String word : words) {
+            if (word.length() > maxLength) {
+                maxLength = word.length();
+                maxWord = word;
+            }
+        }
+
+        System.out.println("Word of max length : " + maxWord);
+    }
+
+    private static String replaceWord(String[] words, String oldWord, String newWord) {
+        StringBuilder sb = new StringBuilder();
+
+        for (String word : words) {
+            if (word.equalsIgnoreCase(oldWord)) {
+                sb.append(newWord);
+            } else {
+                sb.append(word);
+            }
+            sb.append(" ");
+        }
+
+        return sb.toString().trim();
+    }
+}
