3d95b25bfa757090c44d2aeeaaa1156a41c51a9f
Harit Patel <haritpatel789@gmail.com>
2026-01-21 16:30:54 +0530

    [Harit Patel].Add csv files

--- io-programming-practice/gcr-codebase/csv-data-handling/DetectDuplicate.java
+++ io-programming-practice/gcr-codebase/csv-data-handling/DetectDuplicate.java
@@ -6,14 +6,14 @@ import java.util.HashSet;
 public class DetectDuplicate {
 	public static void main(String[] args) {
 		HashSet<Integer> set = new HashSet<>();
-		try(BufferedReader br = new BufferedReader(new FileReader("student.csv"))){
+		try (BufferedReader br = new BufferedReader(new FileReader("student.csv"))) {
 			String line;
-			while((line = br.readLine()) != null) {
+			while ((line = br.readLine()) != null) {
 				String[] columns = line.split(",");
 				int id = Integer.parseInt(columns[0]);
-				if(set.contains(id)) {
-					System.out.println("Id : " + columns[0] + " Name : " + columns[1] + " Age : " + columns[2] + " Marks : "
-							+ columns[3]);
+				if (set.contains(id)) {
+					System.out.println("Id : " + columns[0] + " Name : " + columns[1] + " Age : " + columns[2]
+							+ " Marks : " + columns[3]);
 				} else {
 					set.add(id);
 				}
